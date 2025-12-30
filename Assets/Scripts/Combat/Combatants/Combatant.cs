18a5223b8b13eb5d23bb27458a83c9c4fe19579f
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-12-30 17:48:25 -0600

    Combatant

---
+++ Assets/Scripts/Combat/Combatants/Combatant.cs
@@ -0,0 +1,17 @@
+/*****************************************************************************
+// File Name : Combatant.cs
+// Author : Eli Koederitz
+// Creation Date : 12/30/2025
+// Last Modified : 12/30/2025
+//
+// Brief Description : Abstract class  th
+*****************************************************************************/
+using UnityEngine;
+
+namespace COTB.Combat
+{
+    public abstract class Combatant : MonoBehaviour
+    {
+    
+    }
+}
