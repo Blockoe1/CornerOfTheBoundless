fb6bcc6693647e2158c89d386e7fe2b6e144beb1
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-12-30 18:40:27 -0600

    Update Character.cs

--- Assets/Scripts/Combat/Combatants/Character.cs
+++ Assets/Scripts/Combat/Combatants/Character.cs
@@ -7,11 +7,31 @@
 // Brief Description : Represents a player controlled character in combat.
 *****************************************************************************/
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace COTB.Combat
 {
     public class Character : Combatant
     {
-        
+        [SerializeField] private Command[] debugSkills;
+
+        #region Properties
+        private Command[] Skills
+        {
+            get
+            {
+                return debugSkills;
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="isSelected"></param>
+        public void SetSelected(bool isSelected)
+        {
+
+        }
     }
 }
