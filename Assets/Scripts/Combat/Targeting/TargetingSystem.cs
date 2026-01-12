5c5882c57d7ce4362b0c2ff59c46373e6103bd13
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-01-11 22:10:22 -0600

    stuff

---
+++ Assets/Scripts/Combat/Targeting/TargetingSystem.cs
@@ -0,0 +1,17 @@
+/*****************************************************************************
+// File Name : TargetingSystem.cs
+// Author : Eli Koederitz
+// Creation Date : 1/11/2025
+// Last Modified : 1/11/2025
+//
+// Brief Description : Base class for all targeting systems that simply targets one combatant.
+*****************************************************************************/
+using UnityEngine;
+
+namespace COTB.Combat
+{
+    public abstract class TargetingSystem
+    {
+        
+    }
+}
